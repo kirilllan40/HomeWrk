7b074af014ea198d1227974227ea325d018c2ef9
Kirill <141653827+kirilllan40@users.noreply.github.com>
2024-02-20 22:58:37 +0300

    add template

---
+++ lesson76/main.js
@@ -0,0 +1,14 @@
+const firstName = 'Kirill'
+const surName = 'Tiapkin'
+let age = 24 
+const wasBorn = 'Russia'
+const workPlace = 'beginner developer'
+
+const hello = 'Привет'
+
+
+console.log (firstName, surName)
+console.log (age, wasBorn )
+console.log (workPlace )
+
+alert(hello)
