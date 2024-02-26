c7abb9f8e478c56503eb6cdd359d0c86ceb88139
Kirill <141653827+kirilllan40@users.noreply.github.com>
2024-02-26 17:19:23 +0300

    add lesson 80

---
+++ lesson80/main.js
@@ -0,0 +1,29 @@
+for (let i = 2; i <= 12; i = i + 2 ) {
+    console.log(i)
+}
+
+const pause = '===================='
+console.log(pause)
+
+for (count = 10; count >= 0; count--) {
+    console.log(count)
+}
+
+// ======================================
+
+// let i = 2 
+
+// while(i <= 12) {
+//     i = i + 2;
+//     console.log(i)
+// }
+
+// console.log(pause)
+
+
+// let count = 10
+
+// while (count >= 1) {
+//     count-- 
+//     console.log(count)
+// }
