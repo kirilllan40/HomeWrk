bdf2018ba518bfee39a79a740f3f25a029cf62bb
Kirill <141653827+kirilllan40@users.noreply.github.com>
2024-03-02 00:47:04 +0300

    add lesson 81

---
+++ lesson81/main.js
@@ -0,0 +1,22 @@
+const numbers = [9, 33, 8, 16, 22, 15, 17, 24, 33]
+for (let i = 0; i <  numbers.length; i++) {
+    if ((numbers[i] % 2) == 0) {
+        console.log(numbers[i]);
+    }
+}
+
+
+const raindbow = ['Красный', 'Оранжевый', 'Желтый', 'Зеленый', 'Голубой', 'Синий', 'Фиолетовый'];
+for (let i = 6; i < raindbow.length; i--) {
+    if (i == -1) {
+        break
+    }
+    console.log(raindbow[i])
+}
+
+
+
+
+
+
+
