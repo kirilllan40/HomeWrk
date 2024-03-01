7303919ca5d4082fc60557c513fc66dff800e204
Kirill <141653827+kirilllan40@users.noreply.github.com>
2024-03-02 00:44:06 +0300

    Add files via upload

---
+++ lesson80/main.js
@@ -0,0 +1,30 @@
+for (let i = 2; i <= 12; i = i + 2 ) {
+    console.log(i)
+}
+
+const pause = '===================='
+console.log(pause)
+
+for (count = 10; count >= 1; count--) {
+    console.log(count)
+}
+
+// ======================================
+
+let i = 0
+
+while(i < 12) {
+    i = i + 2;
+    console.log(i)
+    
+}
+
+console.log(pause)
+
+
+let count = 11
+
+while (count > 1) {
+    count-- 
+    console.log(count)
+}
